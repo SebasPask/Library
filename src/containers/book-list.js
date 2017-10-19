08a1d4a3fd8e6b45ae1abc742687ecfeb03b7015
SebasPask <sebaspask@gmail.com>
2017-10-19 21:21:26 +1100

    displaying books

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -7,7 +7,9 @@ class BookList extends Component {
     rederList(){
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item">{book.title} by <a href="http://google.com" >{book.author}</a></li>
+                <li
+                    onClick={() => {this.props.selectBook(book)}}
+                    key={book.title} className="list-group-item">{book.title} by <a href="http://google.com" >{book.author}</a></li>
             );
         });
     }
