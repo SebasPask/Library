10e617ee4cff30f15e4b663f8695b424d89684ac
SebasPask <sebaspask@gmail.com>
2017-10-21 22:04:57 +1100

    adding books sebas , asd

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,7 @@ import BooksReducer from './reducer_book';
 import DvdsReducer from './reducer_dvd';
 const rootReducer = combineReducers({
   books: BooksReducer,
+    book: BooksReducer(sebas,asd)
 });
 
 export default rootReducer;
